acdf2aa720c3b960de5d25d9744631cd9b704e6f
nasser1410 <nasseraymansand@gmail.com>
2024-04-02 00:46:53 +0300

    still working on SideBar

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
+import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="relative">
-      NavBar
+      <NavBar />
       <div className="flex">
-        SlideBar
+        <SideBar />
         <section className="flex min-h-screen felx-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full ">{children}</div>
         </section>
---
+++ components/SideBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { SideBarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const SideBar = () => {
+  const pathName = usePathname();
+  return (
+    <section className="sticky l-0 flex h-screen w-fit felx-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6 ">
+        {SideBarLinks.map((link) => {
+          const isActive =
+            pathName === link.route || pathName.startsWith(link.route);
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                { "bg-blue-1": isActive }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SideBar;
